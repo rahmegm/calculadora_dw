24758bb3fcb6da46cff277094eb7a85aef140bec
rahmegm <gustavo.mr3@puccampinas.edu.br>
2023-08-21 11:44:13 -0300

    escolhendo_operador
    Escolha do operador a ser utilizado pela calculadora

--- calc_dw.py
+++ calc_dw.py
@@ -11,5 +11,13 @@ def div(*args):
 
 
 def calc():
-
-    
+    print("Seja bem vindo a calculadora estudantil!")
+    print("As operações possiveis são:")
+    print("1: Adição")
+    print("2: Subtração")
+    print("3: Multiplicação")
+    print("4: Divisão")
+    resp = int(input("Escolha a operação a ser usada (1 a 4): "))
+    if resp not in [1, 2, 3, 4]:
+        print("Opção não encontrada!")
+        resp = int(input("Escolha a operação a ser usada (1 a 4): "))
