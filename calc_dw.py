b1e5ac64c289918f6932048b0ae87526dc2ac2d7
luissnunes <luisenunessouza@gmail.com>
2023-08-21 11:52:29 -0300

    func_soma
    criando a função soma

--- calc_dw.py
+++ calc_dw.py
@@ -1,4 +1,8 @@
 def soma(*args):
+        resultado = 0
+    for num in args:
+        resultado += num
+    return print(f"O resultado da soma dos valores digitado é: {resultado} ")
 
 
 def sub(*args):
